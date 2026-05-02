fdb343bdbaa87c054b0685de4a261107012e3d7c
cherlyncmu <yourgirlrakroddum@gmail.com>
2026-05-03 00:51:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import streamlit as st
+ import streamlit as st
 ... import numpy as np
 ... import matplotlib.pyplot as plt
 ... 
